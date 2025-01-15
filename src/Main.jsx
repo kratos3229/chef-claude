a36d535ea7983160c9729fee0e919b5ef6b8cfca
Poovanna <poovannakeethianda@gmail.com>
2025-01-15 23:00:50 +0530

    Use map to very basically render list of ingredients

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
+
 export default function Main() {
+  const [ingredients, setIngredients] = useState([]);
+
+  const ingredientEl = ingredients.map((ingredient) => {
+    return <li>{ingredient}</li>;
+  });
+
+  function onSubmitHandler(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const newIngredient = formData.get("ingredient");
+
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+
+    console.log(ingredients);
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form onSubmit={onSubmitHandler} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g oregeno"
           aria-label="Add ingredient"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+      <ul>{ingredientEl}</ul>
     </main>
   );
 }
